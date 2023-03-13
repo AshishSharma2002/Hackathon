2901369dc7992d467016429c1ae45eac98967338
Ashish Sharma <as9779476614@gmail.com>
2023-03-13 16:07:25 +0530

    first

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import Aftnav from './components/Aftnav'
+
+
+
+const App = () => {
+  return (
+    <div className='test-class'>
+      <Navbar></Navbar>
+      <br/><br/><br/>
+      <br/><br/><br/>
+
+      <Aftnav></Aftnav>
+
+    </div>
+
+  )
+}
+
+export default App
+
+
+
