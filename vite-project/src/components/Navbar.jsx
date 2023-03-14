f6bc50776c60b9dd6c079b48e29bfb3b38494360
Ashish Sharma <as9779476614@gmail.com>
2023-03-14 21:17:55 +0530

    nav

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,47 +1,50 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css'
+import image1 from '../assets/signin.png';
+
 const Navbar = () => {
     return (
-    
-           
-                <nav class="navbar navbar-expand-lg bg-body-tertiary  nav-size sticky" >
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li><hr class="dropdown-divider" /></li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled">Disabled</a>
-                                </li>
-                            </ul>
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+        <div>
+
+            <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Digital farmers</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-links">
+                            <li class="nav-item">
+                                <a class="nav-link p-3" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link p-3" href="#">Sellers</a>
+                            </li>
+                            
+                            <li class="nav-item">
+                                <a class="nav-link p-3" href="#">Buyers</a>
+                            </li>
+                        </ul>
+                        <div class='row login'>
+                            <div className=' col-lg-6 col-sm-12'>
+                                <img src={image1} alt="" className='logo' />
+                            </div>
+                            <div className=' col-lg-6 col-sm-12'>
+
+                                <a href="">Login/Sign Up</a>
+                                
+                                
+                            </div>
                         </div>
                     </div>
-                </nav>
+                </div>
+            </nav>
+
+
+        </div>
+
+
     )
 }
-
-export default Navbar
+export default Navbar;
