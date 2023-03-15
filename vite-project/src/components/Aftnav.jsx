2ee1721a7e46cfe06d24fd0ad5d54703b6d99a26
Ashish Sharma <as9779476614@gmail.com>
2023-03-15 22:47:54 +0530

    aftnav

--- vite-project/src/components/Aftnav.jsx
+++ vite-project/src/components/Aftnav.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import './Aftnav2.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import img1 from './assets/img1.jpg'
-import img2 from './assets/img2.jpg'
-import img3 from './assets/img3.jpg'
-import img4 from './assets/img4.jfif'
+import image4 from '../assets/img1.jpg'
+import image5 from '../assets/img2.jpg'
+import image6 from '../assets/img3.jfif'
+import image7 from '../assets/img4.jpg'
+import image8 from '../assets/Build-An-Organic-E-commerce-Platform.png'
 const Aftnav = () => {
   return (
     <div>
       <div className='aftnav'>
         <div className='container-fluid'>
           <div className='row'>
-            <div className=' col col-lg-7 animate-charcter'>
-              <h2>How To Build And E-Commerce Organic Platform That Links Farmers Directly To The Buyers ?</h2>
+            <div className=' col col-lg-6 col-sm-12 animate-charcter'>
+              <h2>How To build An E-Commerce Organic Platform That Links Farmers Directly To The Buyers ?</h2>
             </div>
-            <div className='col col-lg-5'>
-              <img src={img1} alt="" />
-              <img src={img2} alt="" />
-              <img src={img3} alt="" />
-              <img src={img4} alt="" />
+            <div className='col col-lg-6 col-sm-12 '>
+              {/* <img src={image4} alt="" />
+              <img src={image5} alt="" />
+              <img src={image6} alt="" />
+              <img src={image7} alt="" /> */}
+              <img src="image8" alt=""  className=''/>
             </div>
           </div>
         </div>
       </div>
+      <hr />
     </div>
+    
   )
 }
 
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css'
 import image1 from '../assets/signin.png';
+import image2 from '../assets/logo.jpeg';
 
 const Navbar = () => {
     return (
@@ -9,7 +10,9 @@ const Navbar = () => {
 
             <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Digital farmers</a>
+                    <div className='nav-logo'><img src={image2} alt=""  className='logo'/></div>
+                    <a class="navbar-brand" href="#"><b>Digital Farmer</b></a>
+
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -21,20 +24,20 @@ const Navbar = () => {
                             <li class="nav-item">
                                 <a class="nav-link p-3" href="#">Sellers</a>
                             </li>
-                            
+
                             <li class="nav-item">
                                 <a class="nav-link p-3" href="#">Buyers</a>
                             </li>
                         </ul>
                         <div class='row login'>
-                            <div className=' col-lg-6 col-sm-12'>
-                                <img src={image1} alt="" className='logo' />
+                            <div className=' col-lg-6 '>
+                                <img src={image1} alt="" className='signin' />
                             </div>
                             <div className=' col-lg-6 col-sm-12'>
 
-                                <a href="">Login/Sign Up</a>
-                                
-                                
+                                <button type="button" class="btn btn-primary">LogIn</button>    Or   <button type="button" class="btn btn-primary">Create Account</button>
+
+
                             </div>
                         </div>
                     </div>
