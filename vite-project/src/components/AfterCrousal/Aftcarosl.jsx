e8c34ddbb957be87cbfe93299d499acae66beb07
Ashish Sharma <as9779476614@gmail.com>
2023-03-19 16:30:26 +0530

    Aftcarosl

--- vite-project/src/components/AfterCrousal/Aftcarosl.jsx
+++ vite-project/src/components/AfterCrousal/Aftcarosl.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Aftcarosl.css'
+import flowchart from '../assets/Screenshot(22).png'
 
 const Aftcarosl = () => {
     return (
@@ -18,19 +19,23 @@ const Aftcarosl = () => {
 
                         In September 2021, the Union Minister of Agriculture & Farmers Welfare, Mr. Narendra Singh Tomar, announced the initiation of the Digital Agriculture Mission 2021–2025, while signing five memorandum of understandings (MoUs) with CISCO, Ninjacart, Jio Platforms Limited, ITC Limited and NCDEX e-Markets Limited (NeML), to forward digital agriculture through pilot projects. The Digital Agriculture Mission 2021–2025 aims to support and accelerate projects based on new technologies, like AI, block chain, remote sensing and GIS technology and use of drones and robots.  <br /> <br />
 
-                        Cisco developed an Agricultural Digital Infrastructure (ADI) solution in August 2019, that enhances farming and knowledge sharing. This ADI is likely to play a vital role in the data pool that will be created by the Department of Agriculture under the National Agri Stack. The pilot project for this initiative will take place at Kaithal (Haryana) and Morena (Madhya Pradesh). <br /> <br /> 
+                        Cisco developed an Agricultural Digital Infrastructure (ADI) solution in August 2019, that enhances farming and knowledge sharing. This ADI is likely to play a vital role in the data pool that will be created by the Department of Agriculture under the National Agri Stack. The pilot project for this initiative will take place at Kaithal (Haryana) and Morena (Madhya Pradesh). <br /> <br />
 
-                        The Jio Agri (JioKrishi) platform launched in February 2020, digitises the agricultural ecosystem along the entire value chain to empower farmers. The core function of the platform uses stand-alone application data to provide advisory, the advanced functions use data from various sources, feed the data into AI/ML algorithms and provide accurate personalised advice. The pilot project for this initiative will take place at Jalna and Nashik (Maharashtra). <br /> <br /> 
+                        The Jio Agri (JioKrishi) platform launched in February 2020, digitises the agricultural ecosystem along the entire value chain to empower farmers. The core function of the platform uses stand-alone application data to provide advisory, the advanced functions use data from various sources, feed the data into AI/ML algorithms and provide accurate personalised advice. The pilot project for this initiative will take place at Jalna and Nashik (Maharashtra). <br /> <br />
 
 
-                        ITC has proposed to create a personalized ‘Site Specific Crop Advisory’ service to turn conventional crop-level generic advice into a personalised site-specific crop advisory for farmers, using a digital crop monitoring platform, hosted on ITC’s e-Choupal 4.0 digital platform. The pilot project for this initiative will take place at Sehore and Vidisha (Madhya Pradesh). <br /> <br />  
+                        ITC has proposed to create a personalized ‘Site Specific Crop Advisory’ service to turn conventional crop-level generic advice into a personalised site-specific crop advisory for farmers, using a digital crop monitoring platform, hosted on ITC’s e-Choupal 4.0 digital platform. The pilot project for this initiative will take place at Sehore and Vidisha (Madhya Pradesh). <br /> <br />
 
                         The Ministry of Agriculture & Farmers Welfare has developed major digital applications in order to boost technology adoption among farmers: - <br /> <br />
 
-                        <u>National Agriculture Market (eNAM)</u>: - Launched in April 2016, the National Agriculture Market (eNAM) is a pan-India electronic trading portal that links the existing Agricultural Produce Market Committee (APMC) mandis, to create a unified national market for agricultural commodities. eNAM helps farmers sell products without the interference of any brokers or mediators, by generating competitive returns from their investment. <br /> <br />
-                        <u>Direct Benefit Transfer (DBT) Central Agri Portal</u>: - Launched in January 2013, the DBT Agri Portal is a unified central portal for agricultural schemes across the country. The portal helps farmers adopt modern farm machineries through government subsidies. <br /> <br /> 
-                        In June 2021, The Ministry of Agriculture and Farmers Welfare signed an MoU with Microsoft to run a pilot programme for 100 villages in 6 states. Under the MoU, Microsoft will create a ‘Unified Farmer Services Interface’ through its cloud computing services. This is a major part of the ministry’s future plan to create ‘AgriStack’ - a unified platform to provide end-to-end services across the agriculture food value chain to farmers. For this the government is planning to create unique farmer IDs for farmers across the country to integrate it with various <a href="">government schemes</a> and create digital agricultural ecosystems. <br /> <br /> 
+                        <a href="">National Agriculture Market (eNAM)</a>: - Launched in April 2016, the National Agriculture Market (eNAM) is a pan-India electronic trading portal that links the existing Agricultural Produce Market Committee (APMC) mandis, to create a unified national market for agricultural commodities. eNAM helps farmers sell products without the interference of any brokers or mediators, by generating competitive returns from their investment. <br /> <br />
+                        <a href="">Direct Benefit Transfer (DBT) Central Agri Portal</a>: - Launched in January 2013, the DBT Agri Portal is a unified central portal for agricultural schemes across the country. The portal helps farmers adopt modern farm machineries through government subsidies. <br /> <br />
+                        In June 2021, The Ministry of Agriculture and Farmers Welfare signed an MoU with Microsoft to run a pilot programme for 100 villages in 6 states. Under the MoU, Microsoft will create a ‘Unified Farmer Services Interface’ through its cloud computing services. This is a major part of the ministry’s future plan to create ‘AgriStack’ - a unified platform to provide end-to-end services across the agriculture food value chain to farmers. For this the government is planning to create unique farmer IDs for farmers across the country to integrate it with various <a href="" className='change-color'>government schemes</a> and create digital agricultural ecosystems. <br /> <br />
                     </p>
+                    <div className='flowchart'>
+                        <h1>How our website works ?</h1>
+                        <img src={flowchart} alt="" />
+                    </div>
 
                 </div>
 
--- vite-project/src/components/Carousal/Carousal.jsx
+++ vite-project/src/components/Carousal/Carousal.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Carousal.css'
-import picture from '../img2.jpg'
+
 
 
 import picture3 from '../../assets/news2.jpg'
 import picture2 from '../../assets/news1.jpg'
-import picture from '../../assets/news3.jfif'
+import picture5 from '../../assets/news5.avif'
+import picture6 from '../../assets/news4.jfif'
+
+
 
 
 
@@ -19,13 +22,16 @@ const Carousal = () => {
             <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="4600">
-                        <img src={picture} class="d-block w-100" alt="..." />
+                        <img src={picture5} class="d-block w-100 h-30 news1" alt="..." />
                     </div>
                     <div class="carousel-item" data-bs-interval="3600">
-                        <img src={picture2} class="d-block w-100" alt="..." />
+                        <img src={picture2} class="d-block w-100  news2" alt="..." />
                     </div>
                     <div class="carousel-item" data-bs-interval="3600">
-                        <img src={picture3} class="d-block w-100" alt="..." />
+                        <img src={picture3} class="d-block w-100 news3" alt="..." />
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3600">
+                        <img src={picture6} class="d-block w-100 news4" alt="..." />
                     </div>
                 </div>
                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
